69e8727ba9d9dda4848c73bf70a0904099c74be2
machao <frmachao@126.com>
2025-01-13 18:07:33 +0800

    chore: init

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
 import Image from "next/image";
 
+const isAppRouter = (): boolean => {
+  try {
+    // Throws an error if we are not in the App Router context.
+    const { cookies } = require("next/headers");
+    cookies();
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
 export default function Home() {
+  const isAppRouterContext = isAppRouter();
+  console.log("isAppRouterContext:", isAppRouterContext);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
